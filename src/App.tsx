656a030edbfd5b600c0b3766fae9730437096393
zanina <zanina.ala20@gmail.com>
2024-02-29 15:50:16 +0100

    Last code

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import RegistrationForm from './Components/RegistrationForm';
 
 const App: React.FC = () => {
   const handleNext = () => {
-    // Add logic for handling 'Next' button click, e.g., navigate to the next step
     console.log('Next button clicked');
   };
 
   return (
     <div>
       <div style={{ float: 'right' }}>
-        {/* Add background image styling here */}
       </div>
       <RegistrationForm onNext={handleNext} />
     </div>
